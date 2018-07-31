6160a45cd8cd4b36930637f95107c526bffab8bc
Jacob Evans <jacob@dekz.net>
2018-07-31 17:24:19 +0800

    Introduce SignerProviderType
    This allows the developer to indicate the nuanced signer provider. Some have different implementations (trezor, ledger) and others have different implementations (metamask). Breaking the abstraction of eth_sign. EthSign assumes a spec compliant implementation and can be used as a default

--- packages/order-utils/src/order_factory.ts
+++ packages/order-utils/src/order_factory.ts
@@ -1,13 +1,11 @@
-import { ECSignature, SignedOrder } from '@0xproject/types';
+import { SignedOrder, SignerProviderType } from '@0xproject/types';
 import { BigNumber } from '@0xproject/utils';
 import { Provider } from 'ethereum-types';
-import * as ethUtil from 'ethereumjs-util';
 import * as _ from 'lodash';
 
 import { orderHashUtils } from './order_hash';
 import { generatePseudoRandomSalt } from './salt';
 import { ecSignOrderHashAsync } from './signature_utils';
-import { MessagePrefixType } from './types';
 
 export const orderFactory = {
     async createSignedOrderAsync(
@@ -45,26 +43,8 @@ export const orderFactory = {
             expirationTimeSeconds,
         };
         const orderHash = orderHashUtils.getOrderHashHex(order);
-        const messagePrefixOpts = {
-            prefixType: MessagePrefixType.EthSign,
-            shouldAddPrefixBeforeCallingEthSign: false,
-        };
-        const ecSignature = await ecSignOrderHashAsync(provider, orderHash, makerAddress, messagePrefixOpts);
-        const signature = getVRSHexString(ecSignature);
+        const signature = await ecSignOrderHashAsync(provider, orderHash, makerAddress, SignerProviderType.EthSign);
         const signedOrder: SignedOrder = _.assign(order, { signature });
         return signedOrder;
     },
 };
-
-function getVRSHexString(ecSignature: ECSignature): string {
-    const ETH_SIGN_SIGNATURE_TYPE = '03';
-    const vrs = `${intToHex(ecSignature.v)}${ethUtil.stripHexPrefix(ecSignature.r)}${ethUtil.stripHexPrefix(
-        ecSignature.s,
-    )}${ETH_SIGN_SIGNATURE_TYPE}`;
-    return vrs;
-}
-
-function intToHex(i: number): string {
-    const hex = ethUtil.bufferToHex(ethUtil.toBuffer(i));
-    return hex;
-}
